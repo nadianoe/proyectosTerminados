cc23de61fd619dae005d615fc234761c5811ce1d
nmatias <nmatias@dc.uba.ar>
2019-07-06 20:39:21 -0300

    dalee

--- src/main/java/com/nadia/ejemploProyecto/HomeController.java
+++ src/main/java/com/nadia/ejemploProyecto/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class HomeController {
 
-   @GetMapping("/htmls/scheduleGames")
+   @GetMapping("../htmls/scheduleGames")
     public String home(){
        return "scheduleGames.html";
    }
