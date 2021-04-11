705abbe0736f966a57aa1981e97e515f1a3028b0
nadianoemmi <nmatias@dc.uba.ar>
2021-04-11 03:30:59 -0300

    projecto terminadu

--- demo/src/main/java/com/example/demo/PaginitasahreApplication.java
+++ demo/src/main/java/com/example/demo/PaginitasahreApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class PaginitasahreApplication {
 
@@ -10,4 +13,8 @@ public class PaginitasahreApplication {
         SpringApplication.run(PaginitasahreApplication.class, args);
     }
 
+    @RequestMapping("/")
+    public String index() {
+        return "principal";
+    }
 }
